13654cc12fe8d4a74d14e464df6dde103919ef8b
move18 <snoozie@hotmail.se>
2023-04-23 18:40:06 +0200

    Should be okay

--- src/Main.java
+++ src/Main.java
@@ -13,43 +13,60 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    private TextField textField;
-    private Label label;
-    private Label hejLabel;
-    public void start(Stage primaryStage){
+
+    private MenuItem newMap;
+    private MenuItem open;
+    private MenuItem save;
+    private MenuItem saveImg;
+    private MenuItem exit;
+    private Button findPath;
+    private Button showCon;
+    private Button newPlace;
+    private Button newCon;
+    private Button changeCon;
+
+
+    public void start(Stage primaryStage) {
         VBox root = new VBox();
         MenuBar menuBar = new MenuBar();
         Menu fileMenu = new Menu("File");
+         newMap = new MenuItem("New Map");
+         open = new MenuItem("Open");
+         save = new MenuItem("Save");
+         saveImg = new MenuItem("Save Image");
+         exit = new MenuItem("Exit");
+
+        fileMenu.getItems().addAll(newMap, open, save, saveImg, exit);
         menuBar.getMenus().add(fileMenu);
 
 
         FlowPane buttons = new FlowPane();
         buttons.setPadding(new Insets(10));
         buttons.setHgap(10);
-        Button findPath = new Button("Find Path");
-        Button showCon = new Button("Show Connection");
-        Button newPlace = new Button("New Place");
-        Button newCon = new Button("New Connection");
-        Button changeCon = new Button("Change connection");
-        buttons.getChildren().addAll(findPath,showCon,newPlace,newCon,changeCon);
-
+        findPath = new Button("Find Path");
+        showCon = new Button("Show Connection");
+        newPlace = new Button("New Place");
+        newCon = new Button("New Connection");
+        changeCon = new Button("Change connection");
+        buttons.getChildren().addAll(findPath, showCon, newPlace, newCon, changeCon);
 
 
         root.getChildren().add(menuBar);
         root.getChildren().add(buttons);
-        Scene scene = new Scene(root,550, 100);
+        Scene scene = new Scene(root, 550, 100);
         primaryStage.setScene(scene);
         primaryStage.setTitle("PathFinder");
         primaryStage.show();
 
     }
+
     public static void main(String[] args) {
         launch();
     }
-    class Handler implements EventHandler<ActionEvent>{
-        public void handle (ActionEvent event){
-            String name = textField.getText();
-            hejLabel.setText(name);
+
+    class Handler implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent event) {
+
         }
     }
 }
