4fd9d012e99471d7c4713a3c3c2699488ce4c12a
move18 <snoozie@hotmail.se>
2023-04-23 17:56:11 +0200

    Should be okay

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,17 +17,31 @@ public class Main extends Application {
     private Label label;
     private Label hejLabel;
     public void start(Stage primaryStage){
-        Pane root = new FlowPane();
-        root.setStyle("-fx-font-size: 18");
-        label = new Label("Hälsa");
-        hejLabel = new Label("Hej");
-        textField = new TextField();
-        Button button = new Button();
-        root.getChildren().addAll(label,textField,hejLabel,button);
-        Scene scene = new Scene(root,300,200);
-        button.setOnAction(new Handler());
+        VBox root = new VBox();
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        menuBar.getMenus().add(fileMenu);
+
+
+        FlowPane buttons = new FlowPane();
+        buttons.setPadding(new Insets(10));
+        buttons.setHgap(10);
+        Button findPath = new Button("Find Path");
+        Button showCon = new Button("Show Connection");
+        Button newPlace = new Button("New Place");
+        Button newCon = new Button("New Connection");
+        Button changeCon = new Button("Change connection");
+        buttons.getChildren().addAll(findPath,showCon,newPlace,newCon,changeCon);
+
+
+
+        root.getChildren().add(menuBar);
+        root.getChildren().add(buttons);
+        Scene scene = new Scene(root,550, 100);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("PathFinder");
         primaryStage.show();
+
     }
     public static void main(String[] args) {
         launch();
