b711137cedfb9a6a56553c86e4aa395b214b05f1
move18 <snoozie@hotmail.se>
2023-04-28 08:18:06 +0200

    Working on graphics

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -1,25 +1,33 @@
 
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.w3c.dom.events.Event;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.*;
 
 public class PathFinder extends Application {
@@ -29,6 +37,8 @@ public class PathFinder extends Application {
     //
     //
     //
+    private static final String MAP_NAME = "file:europa.gif";
+    private ListGraph<Location> graph;
     private VBox root;
     private Pane map;
     private MenuBar menuBar;
@@ -38,6 +48,7 @@ public class PathFinder extends Application {
     private MenuItem menuSave;
     private MenuItem menuSaveImg;
     private MenuItem menuExit;
+    private FlowPane buttons;
     private Button findPath;
     private Button showCon;
     private Button newPlace;
@@ -53,37 +64,47 @@ public class PathFinder extends Application {
         root = new VBox();
         menuBar = new MenuBar();
         fileMenu = new Menu("File");
+
         menuFileNewMap = new MenuItem("New Map");
-        menuFileNewMap.setOnAction(event -> openMap("file:europa.gif"));
+        menuFileNewMap.setOnAction(event -> openMap(MAP_NAME));
 
         menuFileOpen = new MenuItem("Open");
         menuFileOpen.setOnAction(new OpenMapHandler());
 
         menuSave = new MenuItem("Save");
+        menuSave.setOnAction(new SaveMapHandler());
 
         menuSaveImg = new MenuItem("Save Image");
+        menuSaveImg.setOnAction(new SaveImgHandler());
 
         menuExit = new MenuItem("Exit");
-        menuExit.setOnAction(event -> { // Put this code on exitRequest
+        menuExit.setOnAction(event -> {
             if (changes) {
                 primaryStage.fireEvent(new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST));
+            } else {
+                primaryStage.close();
             }
-        }); // END OF TESTING
+        });
 
 
         fileMenu.getItems().addAll(menuFileNewMap, menuFileOpen, menuSave, menuSaveImg, menuExit);
         menuBar.getMenus().add(fileMenu);
 
 
-        FlowPane buttons = new FlowPane();
+        buttons = new FlowPane();
         buttons.setPadding(new Insets(10));
         buttons.setHgap(10);
         findPath = new Button("Find Path");
         showCon = new Button("Show Connection");
         newPlace = new Button("New Place");
+        newPlace.setOnAction(new NewLocationHandler());
         newCon = new Button("New Connection");
         changeCon = new Button("Change connection");
+        buttons.setAlignment(Pos.CENTER);
         buttons.getChildren().addAll(findPath, showCon, newPlace, newCon, changeCon);
+        buttons.setDisable(true);
+
+
 
 
         root.getChildren().add(menuBar);
@@ -92,8 +113,9 @@ public class PathFinder extends Application {
         primaryStage.setOnCloseRequest(new ExitHandler());
         primaryStage.setScene(scene);
         primaryStage.setTitle("PathFinder");
-        primaryStage.show();
         setIDs();
+        primaryStage.show();
+
 
     }
 
@@ -108,12 +130,14 @@ public class PathFinder extends Application {
         changes = true;
         if (map == null) {
             map = new Pane();
+            map.setDisable(true);
             Image background = new Image(mapName);
             ImageView bg = new ImageView(background);
             map.getChildren().add(bg);
             root.getChildren().add(map);
-            primaryStage.setHeight(background.getHeight() + 110); // 110 is extra pixels by other elements
-            primaryStage.setWidth(background.getWidth() + 15); // 15 is padding to make the map look better
+            primaryStage.setHeight(background.getHeight()  + 110); // 110 is extra pixels by other elements
+            primaryStage.setWidth(background.getWidth() + 15); // 15 is padding to make the map look better in the scene
+            buttons.setDisable(false);
         } else {
             root.getChildren().remove(map);
             map = null;
@@ -133,9 +157,6 @@ public class PathFinder extends Application {
         // add buttons here later
     }
 
-    private boolean mapExists() {
-        return root.getChildren().contains(map);
-    }
 
     class ExitHandler implements EventHandler<WindowEvent> {
         @Override
@@ -158,7 +179,7 @@ public class PathFinder extends Application {
         @Override
         public void handle(ActionEvent actionEvent) {
             Map<String, Location> locations = new HashMap<>();
-            ListGraph<Location> graph = new ListGraph<>();
+            graph = new ListGraph<>();
             if (changes) {
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("Warning!");
@@ -210,15 +231,97 @@ public class PathFinder extends Application {
         }
     }
 
+    class SaveMapHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent event){
+            try {
+                FileWriter file = new FileWriter("europa1.graph");// change this later this is for testing
+                PrintWriter out = new PrintWriter(file);
+                out.println("file:europa.gif");
+                for(Location loc : graph.getNodes()) {
+                    out.format("%s;%.01f;%.01f;",loc.getName(), loc.getX(), loc.getY());
+                }
+                out.println();
+                for (Location loc : graph.getNodes()){
+                    for (Edge<Location> edge : graph.getEdgesFrom(loc)){
+                        out.format("%s;%s;%s;%d\n", loc.getName(), edge.getDestination().getName(), edge.getName(), edge.getWeight());
+                    }
+                }
+                out.close();
+                file.close();
+                changes = false;
+            }catch (FileNotFoundException e){
+                Alert error = new Alert(Alert.AlertType.ERROR);
+                error.setTitle("Error");
+                error.setHeaderText(null);
+                error.setContentText("Could not write to File: europa.graph");
+                error.showAndWait();
+
+            }catch (IOException e){
+                Alert error = new Alert(Alert.AlertType.ERROR);
+                error.setTitle("Error");
+                error.setHeaderText(null);
+                error.setContentText(e.getMessage());
+                error.showAndWait();
+            }
+        }
+    }
+
     private void loadGraphToMap(ListGraph<Location> graph) {
         for (Location loc : graph.getNodes()) {
             map.getChildren().add(loc);
+            Text cityName = new Text(loc.getX() + loc.getRadius(), loc.getY() + loc.getRadius(), loc.getName());
+
+            // x + 5 and y + 15 is moving them a few pixels so  that the text gets away from the nodes, so it is easier to read
+            cityName.setStrokeWidth(5);
+            cityName.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR,12));
+            cityName.setDisable(true);
+            map.getChildren().add(cityName);
+
             for (Edge<Location> edge : graph.getEdgesFrom(loc)) {
                 Line connection = new Line(loc.getX(), loc.getY(), edge.getDestination().getX(), edge.getDestination().getY());
                 connection.setStrokeWidth(2);
                 connection.setDisable(true); // makes lines not clickable on map easier to click nodes
                 map.getChildren().add(connection);
             }
+        }
+    }
+
+    class SaveImgHandler implements EventHandler<ActionEvent>{
+        public void handle(ActionEvent event){
+            try{
+                WritableImage image = root.snapshot(null, null);
+                BufferedImage buffImage = SwingFXUtils.fromFXImage(image, null);
+                ImageIO.write(buffImage, "png", new File("capture.png"));
+            }catch (IOException e){
+                Alert alert = new Alert(Alert.AlertType.ERROR,"" + e.getMessage());
+                alert.showAndWait();
+            }
+        }
+    }
+    class NewLocationHandler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent event){
+            graph = new ListGraph<>();
+            root.cursorProperty().setValue(Cursor.CROSSHAIR);
+            newPlace.setDisable(true);
+            map.setDisable(false);
+            map.setOnMouseClicked(mouseEvent ->{
+                TextInputDialog nameOfLoc = new TextInputDialog("Name");
+                nameOfLoc.setHeaderText(null);
+                nameOfLoc.setContentText("Name of place:");
+                Optional<String> name = nameOfLoc.showAndWait();
+                if (name.isPresent()) {
+                    Location loc = new Location(name.get(), mouseEvent.getX(), mouseEvent.getY());
+                    graph.add(loc);
+                    loadGraphToMap(graph);
+
+                }
+                root.cursorProperty().setValue(Cursor.DEFAULT);
+                map.setDisable(true);
+                newPlace.setDisable(false);
+            });
+
         }
     }
 }
